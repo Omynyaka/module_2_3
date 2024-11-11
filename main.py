da94e7fa87b680028a5d9a6adda8ce46638abeef
Omynyaka <azriel95mail@gmail.com>
2024-11-11 21:13:16 +0800

    module_2_3

---
+++ main.py
@@ -0,0 +1,15 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+zero = 0
+while len(my_list) > zero:
+    if 0 > my_list[zero]:
+        break
+    print(my_list[zero])
+    zero += 1 # Не смог убрать 0 из вывода
+
+
+
+
+
+
+
+
