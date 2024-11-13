b96e5a70d9145a509b2e04ef58023e3770829c4d
Omynyaka <azriel95mail@gmail.com>
2024-11-14 00:35:44 +0800

    module_2_3

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 zero = 0
 while len(my_list) > zero:
-    if 0 > my_list[zero]:
+    if  my_list[zero]>0:
+        print(my_list[zero])
+    if  my_list[zero] < 0:
         break
-    print(my_list[zero])
-    zero += 1 # Не смог убрать 0 из вывода
+
+    zero += 1
 
 
 
